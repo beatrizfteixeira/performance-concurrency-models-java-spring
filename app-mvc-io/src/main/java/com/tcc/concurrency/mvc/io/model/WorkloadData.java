3aaad93ba9fc871aa2c36b0172329edc6e7f95c2
Beatriz Teixeira <biaft03@protonmail.com>
2026-05-10 21:43:02 +0000

    update

--- app-mvc-io/src/main/java/com/tcc/concurrency/mvc/io/model/WorkloadData.java
+++ app-mvc-io/src/main/java/com/tcc/concurrency/mvc/io/model/WorkloadData.java
@@ -1,4 +1,4 @@
-package com.tcc.concurrency.mvc.io.model;
+/* package com.tcc.concurrency.mvc.io.model;
 
 import java.time.LocalDateTime;
 
@@ -56,3 +56,4 @@ public class WorkloadData {
         this.createdAt = createdAt;
     }
 }
+ */
